0af7fc7df015de939bc37ccfafe78c74e325a4c9
HarwordLiu <harwordliu@Gmail.com>
2017-10-16 21:58:49 +0800

    first week init

---
+++ SwiftDemo/FirstWeek/Project/FirstWeek/RootViewControllerTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RootViewControllerTableViewController.swift
+//  FirstWeek
+//
+//  Created by 刘浩 on 2017/10/16.
+//  Copyright © 2017年 HarwordLiu. All rights reserved.
+//
+
+import UIKit
+
+class RootViewControllerTableViewController: UITableViewController {
+    
+    let dataSources = ["First", "Second", "Third", "Fourth"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return dataSources.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rootCellReuseIdentifier", for: indexPath)
+        cell.textLabel?.text = dataSources[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewController = UIViewController(nibName: dataSources[indexPath.row] + "ViewController", bundle: nil)
+        viewController.title = dataSources[indexPath.row];
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
