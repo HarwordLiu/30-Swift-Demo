9cbc266b7ea781743ed3c9b51f84ecad8ee552e7
HarwordLiu <harwordliu@Gmail.com>
2017-10-19 15:38:01 +0800

    数据关联

--- SwiftDemo/FirstWeek/Project/FirstWeek/FirstViewController.swift
+++ SwiftDemo/FirstWeek/Project/FirstWeek/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var inputTextField: UITextField!
     
@@ -20,13 +20,48 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var percentSilder: UISlider!
     
-    
-    
-    
     override func viewDidLoad() {
-
         super.viewDidLoad()
-
+        InitUI()
+    }
+    
+    func InitUI() {
+        inputTextField.clearButtonMode = .whileEditing
+        inputTextField.delegate = self
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        tipLabel.text = getTipLabelTextWith(value: percentSilder.value)
+        tipTotalLabel.text = getTipTotalLabelTextWith(value: percentSilder.value)
+        return true
+    }
+    
+    @IBAction func moveSlider(_ sender: UISlider) {
+        percentLabel.text = getPercentLabelTextWith(value: sender.value)
+        tipLabel.text = getTipLabelTextWith(value: sender.value)
+        tipTotalLabel.text = getTipTotalLabelTextWith(value: sender.value)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        inputTextField.resignFirstResponder()
+    }
+    
+    func getPercentLabelTextWith(value: Float) -> String {
+        return String(format: "Tip%.f%%", value * 100)
+    }
+    
+    func getTipLabelTextWith(value: Float) -> String {
+        guard let total = Float(inputTextField.text!) else {
+            return "$0.00"
+        }
+        return String(format: "$%.2f", value * total)
+    }
+    
+    func getTipTotalLabelTextWith(value: Float) -> String {
+        guard let total = Float(inputTextField.text!) else {
+            return "$0.00"
+        }
+        return String(format: "$%.2f", total * value + total)
     }
 
     override func didReceiveMemoryWarning() {
