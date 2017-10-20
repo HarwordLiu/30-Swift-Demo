2078455c17ff65f348a757ba9989d3f4166e965f
HarwordLiu <harwordliu@Gmail.com>
2017-10-20 14:47:16 +0800

    Firstweek second

--- SwiftDemo/FirstWeek/Project/FirstWeek/SecondViewController.swift
+++ SwiftDemo/FirstWeek/Project/FirstWeek/SecondViewController.swift
@@ -11,32 +11,73 @@ import UIKit
 class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let dataSources = ["A" : ["1234", "2345"],
-                       "B" : ["1234123", "4123412341"]]
+                       "B" : ["1234123", "41234123412341"],
+                       "K" : ["1234413123", "41221343412341"],
+                       "M" : ["1234123", "412341112342342341"],
+                       "N" : ["123414123", "41234123412341"],
+                       "D" : ["123412ASDF3", "41212343412341"],
+                       "F" : ["12341ASDF23", "41212343412341"],
+                       "U" : ["1234ASDF123", "41212343412341"],
+                       "L" : ["123WAER4123", "4121234QWE3412341"],
+                       "Z" : ["123ZSCV4123", "412QWER3412341"],
+                       "P" : ["1234123", "41234QWERSFF12341"],]
+    var dataKeys: Array<String>? = []
+    var dataValues: Array<Array<String>>? = []
     
     var tableView = UITableView(frame: UIScreen.main.bounds, style: .grouped)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initData()
         initUI()
         // Do any additional setup after loading the view.
     }
+    
+    func initData() {
+        dataKeys = dataSources.keys.sorted()
+        for key in dataKeys! {
+            dataValues?.append(dataSources[key]!)
+        }
+    }
+    
     func initUI() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.sectionFooterHeight = 0
+        tableView.sectionHeaderHeight = 30
         view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SecondViewControllerCell")
+        tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "SecondViewControllerHeader")
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return dataSources.keys.count
+        return (dataKeys?.count)!
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataValues![section].count
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSources.values
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "SecondViewControllerHeader")
+        header?.textLabel?.text = dataKeys?[section]
+        return header
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return nil
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SecondViewControllerCell")
+        cell?.textLabel?.text = dataValues![indexPath.section][indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return dataKeys
     }
 
     override func didReceiveMemoryWarning() {
