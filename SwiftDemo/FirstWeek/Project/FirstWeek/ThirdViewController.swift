e38cbd7584a7201a1e8cb403e162a516d3b4e43d
HarwordLiu <harwordliu@Gmail.com>
2017-10-20 22:56:24 +0800

    Update first week third

--- SwiftDemo/FirstWeek/Project/FirstWeek/ThirdViewController.swift
+++ SwiftDemo/FirstWeek/Project/FirstWeek/ThirdViewController.swift
@@ -10,12 +10,55 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    var timer: Timer?
+    
+    var timeData: Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        fireTimer()
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            return
+        }
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+            self.timeData += 0.1
+            self.contentLabel.text = String(format: "%.1f", self.timeData)
+        }
+        
+    }
+    
+    func fireTimer() {
+        if timer == nil {
+            return
+        }
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @IBAction func clickResetBtn(_ sender: UIButton) {
+        fireTimer()
+        timeData = 0.0
+        contentLabel.text = "0.0"
+    }
+    
+    @IBAction func clickStopBtn(_ sender: UIButton) {
+        fireTimer()
+    }
+    
+    @IBAction func clickGoBtn(_ sender: UIButton) {
+        startTimer()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
